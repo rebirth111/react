31ff6a5c3f762d3f01c15227f886fdd3de1f5a7a
2007 <1066324750@qq.com>
2020-08-21 21:29:11 +0800

    装饰器的写法

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
-
-
-import {
-    CountBtn,
-    Counter
-} from './components'
-
-export default class App extends Component{
-    render(){
-        return(
-            <>
-             <CountBtn type="decrement">-</CountBtn>
-             <Counter />
-             <CountBtn type="increment">+</CountBtn>
-            </>
+import Another from './Another'
+class App extends Component {
+    render() {
+        return (
+            <div>
+                App
+                <Another name="组件"/>
+            </div>
         )
     }
 }
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import {CounterProvider} from './counterStore'
 import App from './App'
-render(
-    <CounterProvider>
-    <App />
-    </CounterProvider>,
-    document.querySelector('#root')
-)
+ render(
+     <App/>,
+     document.querySelector('#root')
+ )
