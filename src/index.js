c550768c66ba4caf1fe9aa147a883ce9ef15415b
2007 <1066324750@qq.com>
2020-08-24 23:57:47 +0800

    feat:配置基本的路由

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import {render} from 'react-dom'
-
+import {HashRouter as Router, Route, Switch,Redirect} from 'react-router-dom'
 import App from './App'
 
 
+import {mainRouter} from './routes'
+
 import './index.less'
 render(
-    <App />,
+    <Router>
+        <Switch>
+            <Route path="/admin" render={(routerProps)=>{
+                //TODO权限，需要登录才能访问/admin
+                return <App {...routerProps}/>
+            }} />
+            {
+                mainRouter.map(route=>{
+                    return <Route key={route.pathname} path={route.pathname} component={route.component} />
+                })
+            }
+            <Redirect to="/admin" from="/" exact/>
+            <Redirect to="/404" />
+        </Switch>
+    </Router>,
     document.querySelector('#root')
 )
